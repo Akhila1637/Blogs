4d61c89437fd6c68e56242c01ca319560d887e92
Akhila <akhilavardholuyadav@gmail.com>
2025-04-07 11:59:42 +0530

    blog

---
+++ server/server.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const userRouter = require("./routes/user-routes");
+const blogRouter = require("./routes/blog-routes");
+require("./config/db");
+const mongoose = require("mongoose");
+require('dotenv').config();
+
+const cors = require("cors");
+
+const app = express();
+
+// app.use(cors());
+app.use(
+    cors({
+       //origin:"https://omnify-task-cqej.vercel.app/"
+        origin:"*",
+        methods: "GET,POST,PUT,DELETE",
+        credentials: true 
+    })
+);
+
+app.set("view engine", "ejs");
+app.use(express.json());
+
+app.use("/api/users", userRouter);
+app.use("/api/blogs", blogRouter);
+
+app.use("/api", (req, res, next) => {
+  res.send("hello");
+});
+
+
+
+app.listen(5001, () => console.log("app started at 5001..."));
